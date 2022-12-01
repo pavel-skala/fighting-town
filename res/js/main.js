1e94d88b0c1f9ef223b8b4a620d3caa0b9af5715
pavel-skala <pavelskalamobil@gmail.com>
2022-12-01 13:51:08 +0100

    update design

--- res/js/main.js
+++ res/js/main.js
@@ -1,37 +1,47 @@
+//locations
 const town = document.getElementById("town");
 const battleField = document.getElementById("battleField");
 const shop = document.getElementById("shop");
 const house = document.getElementById("house");
 
+//move buttons
 const goBattle = document.getElementById("goBattle");
 const goShop = document.getElementById("goShop");
 const goBackInShop = document.getElementById("goBackInShop");
 const goHouse = document.getElementById("goHouse");
 const goBackInHouse = document.getElementById("goBackInHouse");
 
-const character = document.getElementById("character");
+//battle
+const player = document.getElementById("player");
 const enemy = document.getElementById("enemy");
 const btnAttack = document.getElementById("btnAttack");
 const btnSuperAttack = document.getElementById("btnSuperAttack");
 const myHealth = document.getElementById("myHealth");
 const enemyHealth = document.getElementById("enemyHealth");
 
+//
+const character = document.getElementsByClassName("character");
+
+//buy buttons
 const btnBuyBasicAttack = document.getElementById("btnBuyBasicAttack");
 const btnBuyHealth = document.getElementById("btnBuyHealth");
 const btnBuySuperAttack = document.getElementById("btnBuySuperAttack");
 const btnBuySuperPowerCouldown = document.getElementById("btnBuySuperPowerCouldown");
 
+//level
 const attackLevel = document.getElementById("attackLevel");
 const healthLevel = document.getElementById("healthLevel");
 const superAttackLevel = document.getElementById("superAttackLevel");
 const couldownLevel = document.getElementById("couldownLevel");
 
+//price
 const attackPrice = document.getElementById("attackPrice");
 const healthPrice = document.getElementById("healthPrice");
 const superAttackPrice = document.getElementById("superAttackPrice");
 const couldownPrice = document.getElementById("couldownPrice");
 const infoCoins = document.getElementById("infoCoins");
 
+//move buttons
 goBattle.onclick = () =>{
     town.style.display = "none";
     battleField.style.display = "block";
@@ -50,10 +60,18 @@ goBackInShop.onclick = () =>{
 goHouse.onclick = () =>{
     house.style.display = "block";
     shop.style.display = "none";
-infoCoins.innerHTML = `Coins: 10`;
 }
 
 goBackInHouse.onclick = () =>{
     shop.style.display = "block";
     house.style.display = "none";
+}
+
+//battle
+btnAttack.onmousedown = () =>{
+    player.style.left = "350px";
+    enemyHealth.innerHTML --;
+}
+btnAttack.onmouseup = () => {
+    player.style.left = "100px";
 }
